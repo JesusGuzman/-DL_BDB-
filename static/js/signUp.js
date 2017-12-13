8c415e982ecc6ded2644e6f241e488a5a161f8da
jesus guzman <jessguzman044@gmail.com>
2017-12-12 21:44:28 -0600

    ad

--- static/js/signUp.js
+++ static/js/signUp.js
@@ -4,9 +4,14 @@ $(function(){
 		$.ajax({
 			url: '/signUp',
 			data: $('form').serialize(),
+			/////////////////////////////
+                        //data: JSON.stringify({'param':{"hello":"world"}}),
+                        dataType: "json",
+                        ////////////////////////////
 			type: 'POST',
 			success: function(response){
 				console.log(response);
+			
 			},
 			error: function(error){
 				console.log(error);
